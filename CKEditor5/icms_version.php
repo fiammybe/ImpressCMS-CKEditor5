72d90c7bb9e20b6fc3483b10287f40614e8cc7a9
fiammybe <david.j@impresscms.org>
2023-05-26 13:22:33 +0200

    First copy of the CKEditor 4 code.

---
+++ CKEditor5/icms_version.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * ImpressCMS CKEditor adapter
+ *
+ * @copyright	The ImpressCMS Project http://www.impresscms.org/
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @package		core
+ * @since		2.0 alpha 5
+ * @author		Steve K <skenow@impresscms.org>
+ * @version		$Id$
+ */
+
+$editorversion = array(
+		'name' => "CKEditor5",
+		'version' => "5",
+		'license' => "MIT see LICENSE",
+		'dirname' => "CKEditor5",
+		'class' => "icmsFormCKEditor5",
+		'file' => "formCKEditor5.php",
+	);
